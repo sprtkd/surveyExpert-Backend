58bc9d9b609fd065ab8dfef9ce4d2ed1a7d0017d
Rajesh Gupta <Rajesh Gupta@LAPTOP-HPHR6TUK>
2019-09-28 01:13:09 +0530

    Created Entity Class for Users table

--- src/main/java/com/infy/surveyExpert/entity/UserEntity.java
+++ src/main/java/com/infy/surveyExpert/entity/UserEntity.java
@@ -1,5 +1,49 @@
 package com.infy.surveyExpert.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="users")
 public class UserEntity {
 
+	@Id
+	private String emailId;
+	private String password;
+	private Integer userId;
+	private String role;
+	
+	public String getEmailId() {
+		return emailId;
+	}
+	
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	public Integer getUserId() {
+		return userId;
+	}
+	
+	public void setUserId(Integer userId) {
+		this.userId = userId;
+	}
+	
+	public String getRole() {
+		return role;
+	}
+	
+	public void setRole(String role) {
+		this.role = role;
+	}
+	
 }
