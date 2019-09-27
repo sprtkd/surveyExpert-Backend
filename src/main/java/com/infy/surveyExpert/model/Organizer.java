d21c3f9b5113b16248ce9c522169bf30a80c265e
Rajesh Gupta <Rajesh Gupta@LAPTOP-HPHR6TUK>
2019-09-28 02:04:34 +0530

    Created Organizer model and class

---
+++ src/main/java/com/infy/surveyExpert/model/Organizer.java
@@ -0,0 +1,15 @@
+package com.infy.surveyExpert.model;
+
+public class Organizer {
+
+	private User user;
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+	
+}
