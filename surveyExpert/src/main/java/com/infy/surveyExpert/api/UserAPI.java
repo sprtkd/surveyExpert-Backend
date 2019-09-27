4161475a5c7c7421dee187d337cf39581b3c63b6
sprtkd <supdey1@gmail.com>
2019-09-27 19:44:55 +0530

    merging

--- surveyExpert/src/main/java/com/infy/surveyExpert/api/UserAPI.java
+++ surveyExpert/src/main/java/com/infy/surveyExpert/api/UserAPI.java
@@ -3,6 +3,7 @@ package com.infy.surveyExpert.api;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,7 +18,7 @@ public class UserAPI {
 	private UserService userService;
 	
 	@RequestMapping(value = "/getprofile/userId", method = RequestMethod.GET)
-	public ResponseEntity<UserModel> getUserProfile() throws Exception {
+	public ResponseEntity<UserModel> getUserProfile(@PathVariable Integer userId) throws Exception {
 		UserModel userModel = userService.getUserProfile(userId);
 		ResponseEntity<UserModel> response = new ResponseEntity<UserModel>(userModel, HttpStatus.OK);
 		return response;
