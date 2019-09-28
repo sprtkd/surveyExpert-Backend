63b20d5e3b9248fab6a75b126a3002f8a5d3e994
Shilpa <usfshilpa@gmail.com>
2019-09-28 01:01:13 -0700

    Repo Added

--- src/main/java/com/infy/surveyExpert/dao/UserDAO.java
+++
@@ -1,7 +0,0 @@
-package com.infy.surveyExpert.dao;
-
-import com.infy.surveyExpert.model.UserModel;
-
-public interface UserDAO {
-	public UserModel getUserProfile(Integer userId) throws Exception;
-}
---
+++ src/main/java/com/infy/surveyExpert/repo/UserRepo.java
@@ -0,0 +1,11 @@
+package com.infy.surveyExpert.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.infy.surveyExpert.entity.UserEntity;
+import com.infy.surveyExpert.model.UserModel;
+@Repository
+public interface UserRepo  extends JpaRepository<UserModel,Integer> {
+	UserEntity findByuserId(Integer userId) ;
+}
