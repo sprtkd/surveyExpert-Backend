b25688ff0c173d18d56d5a180d2d82a9876812d8
Shilpa <usfshilpa@gmail.com>
2019-09-28 07:23:35 -0700

    h

--- src/main/resources/TableScript.sql
+++ src/main/resources/TableScript.sql
@@ -89,3 +89,4 @@ CREATE TABLE DESCRIPTIVE_ANSWERS (
 
 INSERT INTO USERS values ("D","A","U",9,"O");
 insert into survey values(1,1,'2011-12-18 13:17:17','2011-12-18 13:17:17',1);
+insert into organizer values(1,1);
